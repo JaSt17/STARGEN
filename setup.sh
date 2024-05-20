38ab9942c465b2293c4df64a5906734f358648fe
Jaro <ja2332st-s@student.lu.se>
2024-05-20 15:00:16 +0200

    Change the setup scripts to check the conda version before running

--- setup.sh
+++ setup.sh
@@ -3,20 +3,42 @@
 # Name of the environment
 ENV_NAME="myenv"
 
+# Required Conda version
+REQUIRED_CONDA_VERSION="23.7.4"
+
 # List of packages 
 CONDA_PACKAGES=(
     "python=3.12.2"
     "streamlit=1.32.0"
-    "streamlit-folium=0.19.0"
+    "streamlit-folium=0.20.0"
     "folium=0.16.0"
     "pandas=2.2.1"
     "numpy=1.26.4"
-    "scipy=1.12.0"
-    "h3-py=3.7.7"
-    "matplotlib=3.8.0"
+    "scipy=1.13.0"
+    "statsmodels=0.14.1"
+    "h3-py=3.7.6"
+    "haversine=2.8.1"
+    "branca=0.7.1"
+    "matplotlib=3.8.3"
     "openpyxl=3.1.2"
-    "pykrige=1.7.1"
 )
+
+# Function to check if Conda exists and its version
+check_conda() {
+    if ! command -v conda &> /dev/null
+    then
+        echo "Conda not found. Please install Conda version $REQUIRED_CONDA_VERSION."
+        exit 1
+    fi
+
+    CONDA_VERSION=$(conda --version | awk '{print $2}')
+    if [ "$CONDA_VERSION" != "$REQUIRED_CONDA_VERSION" ]; then
+        echo "Warning: Conda version $CONDA_VERSION found. Please use Conda version $REQUIRED_CONDA_VERSION to avoid potential errors."
+    fi
+}
+
+# Check Conda installation and version
+check_conda
 
 # Create the Conda environment with the correct Python version
 echo "Creating environment '$ENV_NAME' with Python version ${CONDA_PACKAGES[0]}"
